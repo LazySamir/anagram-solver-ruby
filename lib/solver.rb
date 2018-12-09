398e90fac01c2d5eb2d609f8ef8f18ab06dd6b5d
Samir <samirsoormally@gmail.com>
2018-12-09 08:48:55 +0000

    refactor: extracted normalise method

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,7 +1,14 @@
 class Solver
   def find_anagrams(word, wordlist = './assets/wordlist.txt')
     anagrams = []
-    File.foreach(wordlist) {|list_word| anagrams << list_word.strip if ((word.downcase.chars.sort.join) == (list_word.downcase.chars.sort.join.strip))}
+    File.foreach(wordlist) {|list_word| anagrams << list_word.strip if ((normalise(word)) == (normalise(list_word).strip))}
     anagrams
   end
+  
+  private
+  
+  def normalise(word)
+    word.downcase.chars.sort.join
+  end
+  
 end
